983131880510dd54f946afda2b74aa7e3b90b38c
SandyOliveira <sandyoliveira828@gmail.com>
2021-03-18 05:05:16 -0300

    CRUD com Flask

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 
+from app.models.tables import User, db
 from app.models.forms import LoginForm
 
 @app.route("/index")
@@ -19,3 +20,45 @@ def login():
         print(form.errors)
 
     return render_template('login.html', form=form)
+
+
+
+
+
+
+@app.route("/create/<info>")
+@app.route("/create", defaults={"info": None})
+def create(info):
+    i = User("Sandy1", "123", "SA", "s@gmail.com")
+    db.session.add(i)
+    db.session.commit()
+    return "Ok"
+
+
+
+@app.route("/select/<info>")
+@app.route("/select", defaults={"info": None})
+def select(info):
+    r = User.query.filter_by(username="Sandy").all()
+    print(r)
+    return "ok"
+
+@app.route("/update/<info>")
+@app.route("/update", defaults={"info": None})
+def update(info):
+    r = User.query.filter_by(username="Sandy").first()
+    r.name = "Sandy Oliveira"
+    db.session.add(r)
+    db.session.commit()
+    print(r)
+    return "ok"
+
+
+@app.route("/delete/<info>")
+@app.route("/delete", defaults={"info": None})
+def delete(info):
+    r = User.query.filter_by(username="Sandy").first()
+    db.session.delete(r)
+    db.session.commit()
+   
+    return "ok"
--- app/models/tables.py
+++ app/models/tables.py
@@ -3,7 +3,7 @@ from app import db
 class User(db.Model):
   __tablename__= "users"
 
-  id = db.Column(db.Interger, primary_key=True)
+  id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String, unique=True)
   password = db.Column(db.String)
   name = db.Column(db.String)
@@ -23,9 +23,9 @@ class User(db.Model):
 class Post(db.Model):
   __tablename__ = "posts"
 
-  id = db.Column(db.Interger, primary_key=True)
+  id = db.Column(db.Integer, primary_key=True)
   content = db.Column(db.Text)
-  user_id = db.Column(db.Interger, db.ForeignKey('users.id'))
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
   user = db.relationship('User', foreign_keys=user_id)
 
@@ -41,9 +41,9 @@ class Post(db.Model):
 class Follow(db.Model):
   __tablename__ = "follow"
 
-  id = db.Column(db.Interger, primary_key=True)
-  user_id = db.Column(db.Interger, db.ForeignKey('users.id'))
-  follower_id = db.Column(db.Interger, db.ForeignKey('users.id'))
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  follower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
   user = db.relationship('User', foreign_keys=user_id)
   follower = db.relationship('User', foreign_keys=follower_id)
