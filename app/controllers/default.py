6b18347a881f513d085f0726c11c4f34318748bf
SandyOliveira <sandyoliveira828@gmail.com>
2021-03-19 03:21:51 -0300

    login e logout - aprendendo Flask

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,10 +1,15 @@
-from flask import render_template, flash
-from flask_login import login_user
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_user, logout_user
 
-from app import app, db
+from app import app, db, lm
 
 from app.models.tables import User
 from app.models.forms import LoginForm
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.filter_by(id=id).first()
 
 @app.route("/index")
 @app.route("/")
@@ -20,12 +25,16 @@ def login():
     if user and user.password == form.password.data:
       login_user(user)
       flash('logado...')
+      return redirect(url_for("index"))
     else:
         flash('Verifique as informações e tente novamente...')
 
   return render_template('login.html', form=form)
   
-
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 @app.route("/create/<info>")
 @app.route("/create", defaults={"info": None})
